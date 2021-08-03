1d6e896d9a0872fb2da81f2fd9c8101c8c31b2e0
Moreno Razzoli <morrolinux@gmail.com>
2021-08-03 18:28:44 +0200

    client keyboard handling

--- KVM/client.py
+++ KVM/client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import socket
-from pynput.mouse import Button, Controller
+from pynput.mouse import Button, Controller as mouseController
+from pynput.keyboard import Key, Controller as kbdController
 
 HOST = '127.0.0.1' 
 PORT = 12346 
@@ -9,13 +10,14 @@ PORT = 12346
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'HELLO')
-    mouse = Controller()
+    mouse = mouseController()
+    keyboard = kbdController()
 
     should_quit = False
 
     while not should_quit:
         data = s.recv(64).decode().split()
-        print('Received', data)
+        # print('Received', data)
         if len(data) == 0:
             break
 
@@ -30,4 +32,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 mouse.scroll(int(data[4]), int(data[5]))
             elif data[3] == 'move':
                 pass # mouse.move(int(data[1]), int(data[2]))
-
+        elif data[0] == 'keyboard':
+            if data[1] == 'press':
+                exec("keyboard.press(" + data[2] + ")")
+            elif data[1] == 'release':
+                exec("keyboard.release(" + data[2] + ")")
