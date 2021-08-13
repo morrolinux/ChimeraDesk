317715b71475dd0419ebff57f2ee777a2085ca49
Moreno Razzoli <morrolinux@gmail.com>
2021-08-13 12:19:23 +0200

    improve client user feedback etc

--- hprdpclient/client.sh
+++ hprdpclient/client.sh
@@ -1,27 +1,55 @@
 #!/bin/bash
 
-if [[ -z $DISPLAY ]]
-then 
-	export DISPLAY=:0.0
-else 
-	zenity --question --title="Remote Control" --text="Allow remote control?" --no-wrap
-	CONTINUE=$? 
+# chech weather we are running from terminal or by double click
+if [[ $TERM != "dumb" ]]; then export UI=0; else export UI=1; fi
+
+# try setting an arbitrary (often correct) DISPLAY variable if not set (ssh?)
+if [[ -z $DISPLAY ]]; then export DISPLAY=:0.0; fi
+
+# try to guess the screen resolution for ffmpeg
+Xres=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+Yres=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+
+# Give the user some feedback when running on UI as shell prompt doesn't pop up
+if [[ $UI -eq 1 ]]; then
+    if [[ $(which zenity) != "" ]]; then 
+        zenity --question --text="Allow remote control?" --no-wrap
+        export ABORT=$? 
+    else
+	export ABORT=0
+    fi
 fi
 
-if [[ $CONTINUE -eq 1 ]]; then exit; fi
+if [[ $ABORT -eq 1 ]]; then exit; fi
 
+# Launch KVM client process and store its PID for later termination
 $APPDIR/usr/bin/python -m client &
+kvmpid=$!
 
-sleep 1
-
-if [[ ! -f ~/.config/hprdpvideo.sh ]]; then 
-
-cat > ~/.config/hprdpvideo.sh <<\EOF
-ffmpeg -f x11grab -draw_mouse 0 -s 1920x1080 -framerate 30 -i $DISPLAY  -c:v libx264 -preset medium -profile high -pix_fmt yuv420p -tune zerolatency -b:v 2000K -minrate 2000K -maxrate 2000K -bufsize 512k -f mpegts tcp://127.0.0.1:12345
-EOF
-
+# Check weather the KVM process is running, terminate if not.
+sleep 1 && kill -0 $kvmpid
+kvm_error=$?
+if [[ $kvm_error -eq 0 ]]; then
+    notify-send "Screen sharing is starting..."
+else
+    notify-send "Screen sharing could not start"
+    exit 1
 fi
 
+# If no ffmpeg config file is found, generate a default one
+if [[ ! -f ~/.config/hprdpvideo.sh ]]; then 
+    echo "echo \$BASHPID > /tmp/ffmpeg.pid; ffmpeg -f x11grab -draw_mouse 0 -s ${Xres}x${Yres} -framerate 30 -i $DISPLAY  -c:v libx264 -preset medium -profile high -pix_fmt yuv420p -tune zerolatency -b:v 2000K -minrate 2000K -maxrate 2000K -bufsize 512k -f mpegts tcp://127.0.0.1:12345" > ~/.config/hprdpvideo.sh
+fi
+
+# Give the user the option to terminate screen sharing
+if [[ $UI -eq 1 ]]; then
+    if [[ $(which zenity) != "" ]]; then 
+	    (zenity --info --text="Close this window to stop sharing" --no-wrap; pkill -TERM -P $(cat /tmp/ffmpeg.pid))&
+    fi
+fi
+
+# Launch ffmpeg screen sharing
 bash ~/.config/hprdpvideo.sh
 
-zenity --info --title="Terminating" --text="Screen sharing ended" --no-wrap
+# Terminate KVM process when ffmpeg stops and feedback the user
+kill $kvmpid && notify-send "Screen sharing terminated"
